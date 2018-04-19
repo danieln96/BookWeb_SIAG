51ab002fa704d8ff4d86ce958776982eba3201ad
Daniel Niczyporuk <niczyporuk96@interia.pl>
2018-04-19 12:10:41 +0000

    Sprawdzenie sesji zalogowania uzytkownika

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :check_session
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -8,5 +9,17 @@ class ApplicationController < ActionController::Base
   end
   def logged_in?
     !!current_user
+  end
+  def check_session
+    if session[:user_id]
+      if (session[:expires_at] < Time.current)
+        session[:user_id] = nil
+        session[:expires_at] = nil
+        flash[:info] = "Sesja wygasła. Zaloguj się ponownie..."
+        redirect_to login_path
+      else
+        session[:expires_at] = Time.current + 20.minutes
+      end
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
+        session[:expires_at] = Time.current + 20.minutes
         flash[:success] = "Zostałeś prawidłowo zalogowany"
         redirect_to root_path
     else
